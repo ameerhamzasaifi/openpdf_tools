b64fd9cd50f58ae8a07b14f5a17c253d50cacc1d
Ameer Hamza Saifi <dev.ameerhamzasaifi@gmail.com>
2026-02-22 16:43:18 +0530

    issue is still

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,30 +1,7 @@
-plugins {
-    id("org.gradle.toolchains.llvm-compiler") version "0.1.0" apply false
-}
-
 allprojects {
     repositories {
         google()
         mavenCentral()
-    }
-    
-    // Configure JVM toolchain for all projects
-    plugins.withId("com.android.application") {
-        configure<com.android.build.gradle.BaseExtension> {
-            compileOptions {
-                sourceCompatibility = JavaVersion.VERSION_11
-                targetCompatibility = JavaVersion.VERSION_11
-            }
-        }
-    }
-    
-    plugins.withId("com.android.library") {
-        configure<com.android.build.gradle.LibraryExtension> {
-            compileOptions {
-                sourceCompatibility = JavaVersion.VERSION_11
-                targetCompatibility = JavaVersion.VERSION_11
-            }
-        }
     }
 }
 
@@ -47,7 +24,7 @@ subprojects {
     plugins.withId("kotlin-android") {
         configure<org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension> {
             compilerOptions {
-                jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
+                jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
             }
         }
     }
