327ad588a0539f3f00e0d68e53c7cb4e9904abc7
Ameer Hamza Saifi <dev.ameerhamzasaifi@gmail.com>
2026-03-24 11:46:52 +0530

    android build error fixed

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
 
     defaultConfig {
         applicationId = "com.ahsmobilelabs.openpdf_tools"
-        minSdk = flutter.minSdkVersion
+        minSdk = 26
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
